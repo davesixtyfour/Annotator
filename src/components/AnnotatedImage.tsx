e31a0698435da997ae0218946d582d871d8f1bce
Dave64 <176170967+davesixtyfour@users.noreply.github.com>
2024-08-25 17:27:11 -0700

    Scale image to fit within container

--- src/components/AnnotatedImage.tsx
+++ src/components/AnnotatedImage.tsx
@@ -13,39 +13,41 @@ type AnnotatedImageProps = {
 
 export default function AnnotatedImage({ annotations, height, src, width }: AnnotatedImageProps) {
   const [scale, setScale] = useState({ x: 1, y: 1 });
+  const [imageSize, setImageSize] = useState({ width: 0, height: 0 });
 
   useEffect(() => {
     const img = new Image();
     img.onload = () => {
       const imageAspectRatio = img.width / img.height;
-      const stageAspectRatio = width / height;
+      const containerAspectRatio = width / height;
 
-      let newScale: number;
-      if (stageAspectRatio > imageAspectRatio) {
-        // Stage is wider, scale based on height
-        newScale = height / img.height;
+      let newScale: { x: number; y: number };
+
+      if (containerAspectRatio > imageAspectRatio) {
+        // Container is wider, scale based on height
+        newScale = { x: height / img.height, y: height / img.height };
       } else {
-        // Stage is taller or equal to image, scale based on width
-        newScale = width / img.width;
+        // Container is taller or equal, scale based on width
+        newScale = { x: width / img.width, y: width / img.width };
       }
 
-      setScale({ x: newScale, y: newScale });
+      setScale(newScale);
+      setImageSize({ width: img.width * newScale.x, height: img.height * newScale.y });
     };
     img.src = src;
   }, [src, width, height]);
 
   return (
     <Stage width={width} height={height}>
-      <Container>
-        <Sprite
-          image={src}
-          width={width}
-          height={height}
-          scale={scale}
-          anchor={0.5}
-          x={width / 2}
-          y={height / 2}
-        />
+      <Container
+        scale={scale}
+        width={imageSize.width}
+        height={imageSize.height}
+        x={width / 2}
+        y={height / 2}
+        anchor={0.5}
+      >
+        <Sprite image={src} anchor={0.5} />
         {annotations.map((annotation) => (
           <React.Fragment key={annotation.id}>
             {annotation.bbox && <BoundingBox bbox={annotation.bbox} />}
